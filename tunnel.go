6a2bbee9d981edd8c0f6f63fff967fc4ce517c96
Yilun <zyl.skysniper@gmail.com>
2020-08-13 18:05:26 -0700

    Will listen to both tuna and relay sessions for tuna mode
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- tunnel.go
+++ tunnel.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"net"
 	"strings"
+	"sync"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/nknorg/ncp-go"
 	nkn "github.com/nknorg/nkn-sdk-go"
 	ts "github.com/nknorg/nkn-tuna-session"
@@ -14,6 +16,7 @@ import (
 
 type nknDialer interface {
 	Dial(addr string) (net.Conn, error)
+	Close() error
 }
 
 // Tunnel is the tunnel client struct.
@@ -22,9 +25,12 @@ type Tunnel struct {
 	to        string
 	fromNKN   bool
 	toNKN     bool
-	nknDialer nknDialer
-	listener  net.Listener
+	dialer    nknDialer
+	listeners []net.Listener
 	verbose   bool
+
+	lock     sync.RWMutex
+	isClosed bool
 }
 
 // NewTunnel creates a Tunnel client with given options.
@@ -75,25 +81,28 @@ func NewTunnel(numClients int, seed []byte, identifier, from, to string, session
 		}
 	}
 
-	var listener net.Listener
+	listeners := make([]net.Listener, 0, 2)
 
 	if fromNKN {
 		if tuna {
-			listener = c
+			listeners = append(listeners, c)
 			err = c.Listen(nil)
-		} else {
-			listener = m
-			err = m.Listen(nil)
+			if err != nil {
+				return nil, err
+			}
 		}
+		listeners = append(listeners, m)
+		err = m.Listen(nil)
 		if err != nil {
 			return nil, err
 		}
-		from = listener.Addr().String()
+		from = m.Addr().String()
 	} else {
-		listener, err = net.Listen("tcp", from)
+		listener, err := net.Listen("tcp", from)
 		if err != nil {
 			return nil, err
 		}
+		listeners = append(listeners, listener)
 	}
 
 	log.Println("Listening at", from)
@@ -103,8 +112,8 @@ func NewTunnel(numClients int, seed []byte, identifier, from, to string, session
 		to:        to,
 		fromNKN:   fromNKN,
 		toNKN:     toNKN,
-		nknDialer: dialer,
-		listener:  listener,
+		dialer:    dialer,
+		listeners: listeners,
 		verbose:   verbose,
 	}
 
@@ -113,37 +122,86 @@ func NewTunnel(numClients int, seed []byte, identifier, from, to string, session
 
 func (t *Tunnel) dial(addr string) (net.Conn, error) {
 	if t.toNKN {
-		return t.nknDialer.Dial(addr)
+		return t.dialer.Dial(addr)
 	}
 	return net.Dial("tcp", addr)
 }
 
 // Start starts the tunnel and will return on error.
 func (t *Tunnel) Start() error {
-	for {
-		fromConn, err := t.listener.Accept()
-		if err != nil {
-			return err
-		}
+	errChan := make(chan error, 2)
+	for _, listener := range t.listeners {
+		go func(listener net.Listener) {
+			for {
+				fromConn, err := listener.Accept()
+				if err != nil {
+					errChan <- err
+					return
+				}
 
-		if t.verbose {
-			log.Println("Accept from", fromConn.RemoteAddr())
-		}
+				if t.verbose {
+					log.Println("Accept from", fromConn.RemoteAddr())
+				}
 
-		go func(fromConn net.Conn) {
-			toConn, err := t.dial(t.to)
-			if err != nil {
-				log.Println(err)
-				return
+				go func(fromConn net.Conn) {
+					toConn, err := t.dial(t.to)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+
+					if t.verbose {
+						log.Println("Dial to", toConn.RemoteAddr())
+					}
+
+					pipe(fromConn, toConn)
+				}(fromConn)
 			}
-
-			if t.verbose {
-				log.Println("Dial to", toConn.RemoteAddr())
-			}
-
-			pipe(fromConn, toConn)
-		}(fromConn)
+		}(listener)
 	}
+
+	err := <-errChan
+
+	if t.IsClosed() {
+		return nil
+	}
+
+	t.Close()
+
+	return err
+}
+
+func (t *Tunnel) IsClosed() bool {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+	return t.isClosed
+}
+
+func (t *Tunnel) Close() error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	if t.isClosed {
+		return nil
+	}
+
+	var errs error
+
+	err := t.dialer.Close()
+	if err != nil {
+		errs = multierror.Append(errs, err)
+	}
+
+	for _, listener := range t.listeners {
+		err = listener.Close()
+		if err != nil {
+			errs = multierror.Append(errs, err)
+		}
+	}
+
+	t.isClosed = true
+
+	return errs
 }
 
 func pipe(a, b net.Conn) {
